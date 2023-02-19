73e00c7772eb918aa3b6cceb94cbefba0ff4848a
Ashitosh <kambleashitosh07@gmail.com>
2023-02-19 21:26:49 +0530

    initial commit

---
+++ client/app.js
@@ -0,0 +1,155 @@
+import bot from './assets/bot2.png'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval
+
+function loader(element) {
+  element.textContent = ''
+
+  loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
+    element.textContent += '.'
+
+    // If the loading indicator has reached three dots, reset it
+    if (element.textContent === '....') {
+      element.textContent = ''
+    }
+  }, 300)
+}
+
+function typeText(element, text) {
+  let index = 0
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index)
+      index++
+    } else {
+      clearInterval(interval)
+    }
+  }, 20)
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+  const timestamp = Date.now()
+  const randomNumber = Math.random()
+  const hexadecimalString = randomNumber.toString(16)
+
+  return `id-${timestamp}-${hexadecimalString}`
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+}
+
+const submitButton = form.querySelector('button[type="submit"]')
+const promptInput = form.querySelector('textarea[name="prompt"]')
+const sendIcon = submitButton.querySelector('img')
+
+const toggleButtonState = () => {
+  const inputValue = promptInput.value.trim()
+  const isInputEmpty = !inputValue
+
+  if (isInputEmpty) {
+    submitButton.disabled = true
+    sendIcon.src = 'assets/bt-dis.png'
+  } else {
+    submitButton.disabled = false
+    sendIcon.src = 'assets/send.svg'
+  }
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  const button = form.querySelector('button[type="submit"]')
+  const promptInput = form.querySelector('textarea[name="prompt"]')
+  const promptValue = promptInput.value.trim()
+
+  if (promptValue) {
+    // Disable the button and show loading gif
+    button.disabled = true
+    sendIcon.src = 'assets/giphy.gif'
+
+    // to clear the textarea input
+    form.reset()
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, ' ', uniqueId)
+
+    // to focus scroll to the bottom
+    chatContainer.scrollTop = chatContainer.scrollHeight
+
+    // specific message div
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+    const response = await fetch('http://localhost:5000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt'),
+      }),
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = ' '
+
+    if (response.ok) {
+      const data = await response.json()
+      const parsedData = data.bot.trim() // trims any trailing spaces/'\n'
+
+      typeText(messageDiv, parsedData)
+    } else {
+      const err = await response.text()
+
+      messageDiv.innerHTML = 'Something went wrong'
+      alert(err)
+    }
+
+    // Re-enable the button and hide loading gif
+    button.disabled = false
+    sendIcon.src = 'assets/send.svg'
+  } else {
+    // disable button and change image
+    button.disabled = true
+    sendIcon.src = 'assets/bt-dis.png'
+  }
+}
+
+promptInput.addEventListener('input', toggleButtonState)
+toggleButtonState() // call the function once on page load
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e)
+  }
+})
---
+++ client/script.js
@@ -0,0 +1,284 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js'
+import {
+  getDatabase,
+  set,
+  ref,
+  update,
+} from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  OAuthProvider,
+  GoogleAuthProvider,
+  getRedirectResult,
+  signInWithPopup,
+  FacebookAuthProvider,
+} from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js'
+
+// import { getAuth, signInWithPopup, FacebookAuthProvider } from 'firebase/auth'
+
+const sign_in_btn = document.querySelector('#sign-in-btn')
+const sign_up_btn = document.querySelector('#sign-up-btn')
+const container = document.querySelector('.container')
+
+sign_up_btn.addEventListener('click', () => {
+  container.classList.add('sign-up-mode')
+})
+
+sign_in_btn.addEventListener('click', () => {
+  container.classList.remove('sign-up-mode')
+})
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCJmTVuUoauqWF5rfV3xZoBesVCJPBn1zA',
+
+  authDomain: 'gamer-ff984.firebaseapp.com',
+
+  projectId: 'gamer-ff984',
+
+  storageBucket: 'gamer-ff984.appspot.com',
+
+  messagingSenderId: '377722972397',
+
+  appId: '1:377722972397:web:11a0eccdcc659498af44e4',
+  //yout config code
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const auth = getAuth()
+
+//sigin with microsoft
+
+microsoftLogin.addEventListener('click', (e) => {
+  const provider = new OAuthProvider('microsoft.com')
+  provider.setCustomParameters({
+    // Force re-consent.
+    prompt: 'consent',
+    // Target specific email with login hint.
+    login_hint: 'user@firstadd.onmicrosoft.com',
+  })
+  provider.addScope('mail.read')
+  provider.addScope('calendars.read')
+  const auth = getAuth()
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // User is signed in.
+      // IdP data available in result.additionalUserInfo.profile.
+
+      // Get the OAuth access token and ID Token
+      const credential = OAuthProvider.credentialFromResult(result)
+      const accessToken = credential.accessToken
+      const idToken = credential.idToken
+    })
+    .catch((error) => {
+      // Handle error.
+    })
+})
+
+//sigin with apple
+
+appleLogin.addEventListener('click', (e) => {
+  const provider = new OAuthProvider('apple.com')
+  provider.addScope('email')
+  provider.addScope('name')
+  provider.setCustomParameters({
+    // Localize the Apple authentication screen in French.
+    locale: 'fr',
+  })
+  const auth = getAuth()
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user
+
+      // Apple credential
+      const credential = OAuthProvider.credentialFromResult(result)
+      const accessToken = credential.accessToken
+      const idToken = credential.idToken
+
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      const email = error.customData.email
+      // The credential that was used.
+      const credential = OAuthProvider.credentialFromError(error)
+
+      // ...
+    })
+})
+
+// SIGN IN WITH GOOGLE
+
+googleLogin.addEventListener('click', (e) => {
+  const auth = getAuth()
+  const provider = new GoogleAuthProvider()
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
+
+  auth.languageCode = 'it'
+  // To apply the default browser preference instead of explicitly setting it.
+  // firebase.auth().useDeviceLanguage();
+
+  provider.setCustomParameters({
+    login_hint: 'user@example.com',
+  })
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result)
+      const token = credential.accessToken
+      // The signed-in user info.
+      const user = result.user
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      const email = error.customData.email
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error)
+      // ...
+    })
+})
+
+// signup with facebook
+
+facebookLogin.addEventListener('click', (e) => {
+  const auth = getAuth()
+  const provider = new FacebookAuthProvider()
+  provider.addScope('user_birthday')
+
+  auth.languageCode = 'it'
+  // To apply the default browser preference instead of explicitly setting it.
+  // firebase.auth().useDeviceLanguage();
+  provider.setCustomParameters({
+    display: 'popup',
+  })
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user
+
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result)
+      const accessToken = credential.accessToken
+
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      const email = error.customData.email
+      // The AuthCredential type that was used.
+      const credential = FacebookAuthProvider.credentialFromError(error)
+
+      // ...
+    })
+})
+
+//signup with Email and Password
+signUp.addEventListener('click', (e) => {
+  var email = document.getElementById('email').value
+  var password = document.getElementById('password').value
+  var username = document.getElementById('username').value
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user
+
+      set(ref(database, 'users/' + user.uid), {
+        username: username,
+        email: email,
+      })
+
+      alert('user created!')
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+
+      alert(errorMessage)
+      // ..
+    })
+})
+
+login.addEventListener('click', (e) => {
+  var email = document.getElementById('email').value
+  var password = document.getElementById('password').value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user
+
+      const dt = new Date()
+      update(ref(database, 'users/' + user.uid), {
+        last_login: dt,
+      })
+
+      alert('User loged in!')
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+
+      alert(errorMessage)
+    })
+})
+
+const user = auth.currentUser
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid
+    //bla bla bla
+    // alert('user alreagy signed in')
+    // ...
+  } else {
+    // User is signed out
+    // alert('user has logged out')
+    // ...
+    //bla bla bla
+  }
+})
+
+// logout.addEventListener('click', (e) => {
+//   signOut(auth)
+//     .then(() => {
+//       // Sign-out successful.
+//       alert('user loged out')
+//     })
+//     .catch((error) => {
+//       // An error happened.
+//       const errorCode = error.code
+//       const errorMessage = error.message
+
+//       alert(errorMessage)
+//     })
+// })
